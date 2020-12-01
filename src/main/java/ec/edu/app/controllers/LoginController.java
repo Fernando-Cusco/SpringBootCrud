4011a9ac9e99acd23076244192a147c18d0ec3b0
Fernando Cusco <bcuscom@est.ups.edu.ec>
2020-12-01 18:09:41 -0500

    [add] LoginView

--- src/main/java/ec/edu/app/controllers/LoginController.java
+++ src/main/java/ec/edu/app/controllers/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.security.Principal;
@@ -12,13 +13,21 @@ import java.security.Principal;
 public class LoginController {
 
     @GetMapping(value = "/login")
-    public String login(Model model, Principal principal, RedirectAttributes flash) {
+    public String login(@RequestParam(value = "error", required = false) String error, @RequestParam(value = "logout", required = false) String logout,Model model, Principal principal, RedirectAttributes flash) {
         if(principal != null) {
             flash.addFlashAttribute("info", "Ya haz iniciado sesión.");
             return "redirect:/";
         }
+        if(error != null) {
+            model.addAttribute("danger", "Error al iniciar sesión, verifica las credenciales");
+        }
+
+        if(logout != null) {
+            model.addAttribute("success", "Haz cerrado sesión con exito");
+        }
         model.addAttribute("titulo", "Inicio de Sesión");
         return "login";
     }
+
 
 }
